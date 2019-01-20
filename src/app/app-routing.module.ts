37c5319669515c5d6064eccd383d77ef0c80210c
dan <daniyal@wellyes.ru>
2019-01-20 18:02:19 +0300

    - Start commit. Created header, main block, navbar.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainInfoComponent } from './main-info/main-info.component';
+import { AboutComponent } from './about/about.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SavingComponent } from './saving/saving.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'main', component: MainInfoComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'instructions', component: InstructionsComponent},
+  {path: 'contacts', component: ContactsComponent},
+  {path: 'saving', component: SavingComponent},
+  { path: '**', component: MainInfoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainInfoComponent } from './main-info/main-info.component';
+import { AboutComponent } from './about/about.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { SavingComponent } from './saving/saving.component';
+import { LeftBlockComponent } from './left-block/left-block.component';
+import { RightBlockComponent } from './right-block/right-block.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    MainInfoComponent,
+    AboutComponent,
+    InstructionsComponent,
+    ContactsComponent,
+    SavingComponent,
+    LeftBlockComponent,
+    RightBlockComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
