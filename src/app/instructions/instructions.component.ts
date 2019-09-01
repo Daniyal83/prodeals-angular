dc43d1d6ef7c000d4b3b67b30da21ffe4fd5962a
dan <daniyal@wellyes.ru>
2019-09-01 12:56:51 +0300

    - Added instructions

--- src/app/instructions/instructions.component.ts
+++ src/app/instructions/instructions.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs/Observable';
-import { Store } from '@ngrx/store';
-import { Phone } from './../models/Phone';
-import * as ProductStore from '../store';
-import { getProductStateProducts, getProductStateSearchResult } from '../store';
 
 @Component({
   selector: 'app-instructions',
@@ -11,28 +6,8 @@ import { getProductStateProducts, getProductStateSearchResult } from '../store';
   styleUrls: ['./instructions.component.scss']
 })
 export class InstructionsComponent implements OnInit {
-  products: Observable<Phone[]>;
-  searchResult: Observable<Phone[]>;
-  newProduct: Phone = {
-    title: "",
-    category: "",
-    brand: "",
-    price: 0,
-    colors: [],
-    photo: "../../assets/images/phones/redmiNote7.jpg",
-    special: false,
-  };
-  colors: string;
-
-  constructor(private store: Store<ProductStore.state>) {
-    this.products = store.select(getProductStateProducts);
-    this.searchResult = store.select(getProductStateSearchResult);
-  }
-
-  addProduct(obj) {
-    const prod = {...obj, colors: [this.colors]};
-    this.store.dispatch(new ProductStore.AddProduct({...prod}))
-  }
+  
+  constructor() {  }
 
   ngOnInit() {
   }
