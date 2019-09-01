d17b8c51a0b830c7c85d710c7fa397d9376c05c0
dan <daniyal@wellyes.ru>
2019-09-01 13:59:33 +0300

    - Added links to product details in search results

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Router } from '@angular/router';
 import * as ProductStore from '../../store';
 import { Observable } from 'rxjs';
 import { Phone } from '../../models/Phone';
@@ -13,8 +14,15 @@ import { getProductStateSearchResult } from '../../store';
 export class SearchResultComponent implements OnInit {
   products: Observable<Phone[]>
 
-  constructor(private store: Store<ProductStore.state>) { 
+  constructor(
+      private store: Store<ProductStore.state>,
+      private router: Router) { 
     this.products = store.select(getProductStateSearchResult);
+  }
+
+  showDetails(value: string, prod: Phone) {
+    this.store.dispatch(new ProductStore.SetProductDetails(prod));
+    this.router.navigate(['/product-details'])
   }
 
   ngOnInit() {
