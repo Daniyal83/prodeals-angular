c4d2dae58137d3564fba923b54399f73d944abf0
dan <daniyal@wellyes.ru>
2019-09-14 21:31:33 +0300

    - Added temporary site developing message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { trigger, transition, animate, style } from '@angular/animations'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    animations: [
+        trigger('slideInOut', [
+            transition(':enter', [
+            style({transform: 'translateY(-100%)'}),
+            animate('400ms ease-in', style({transform: 'translateY(0%)'}))
+            ]),
+            transition(':leave', [
+            animate('500ms ease-in', style({transform: 'translateY(-100%)'}))
+            ])
+        ])
+    ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'prodeals';
+  isSiteWorkRemain: Boolean = true;
+
+  ngOnInit() {
+    setTimeout(() => {
+        this.isSiteWorkRemain = false;
+    }, 4000)
+  }
 }
