1e05c967e79480c9df120e9e6b707420af250e17
dan <daniyal@wellyes.ru>
2019-09-15 17:47:36 +0300

    - Added google ads for right block

--- src/app/right-block/right-block.component.ts
+++ src/app/right-block/right-block.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-right-block',
   templateUrl: './right-block.component.html',
   styleUrls: ['./right-block.component.scss']
 })
-export class RightBlockComponent implements OnInit {
+export class RightBlockComponent implements AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    try {
+        (window["adsbygoogle"] = window["adsbygoogle"] || []).push({});
+    } catch(err) {
+        console.log("google ads error: ", err)
+    }
   }
 
 }
