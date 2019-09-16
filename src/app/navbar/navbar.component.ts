42e09705582531a9c202dc6cd41f72aa0de9851b
dan <daniyal@wellyes.ru>
2019-09-16 11:50:08 +0300

    - Burger menu for mobile devices added

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+    isOpened: Boolean = false
+    constructor() { }
 
-  constructor() { }
+    toggleMenu() {
+        this.isOpened = !this.isOpened;
+    }
 
-  ngOnInit() {
-  }
+    clickedOutside(e: Event) {
+        this.isOpened = false;
+    }
+
+    ngOnInit() {
+    }
 
 }
